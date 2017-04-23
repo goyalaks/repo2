aaeb8e53826fb49e08e3fe49753431502e6d009f
goyal <goyal@192.168.0.2>
2017-04-23 23:39:26 +0530

    jk

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -9,12 +9,9 @@ public class Main {
 	}
 
 	private static void displayGreeting() {
-<<<<<<< HEAD
-		System.out.println("alkshay Hello Happy Sales People!");
-=======
-		System.out.println("akshay Hello Happy Sales People!");
->>>>>>> refs/heads/master
+		{
 		System.out.println("This app shows sales data.");
 		System.out.println("--------------------------");
+	}
 	}
 }
